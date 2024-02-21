815ee2a3892492ea57bbc578282137912f98f01f
mali194 <muhammadabdullahali1@gmail.com>
2024-02-21 10:47:53 +0000

    task3and4

---
+++ models/article.models.js
@@ -0,0 +1,10 @@
+const db = require('../db/connection');
+
+exports.selectArticlesById = (article_id) => {
+   return db.query( 
+    `SELECT * FROM articles
+     WHERE article_id = $1`,
+     [article_id])
+     .then((result) => {
+        return result.rows[0]})
+ }
